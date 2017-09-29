e904619d7095064c92eaadd22b1a456614aabead
Arfian Adam <arfianadam@yahoo.com>
2017-09-29 20:00:46 +0700

    edited site information

--- src/config.js
+++ src/config.js
@@ -15,21 +15,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'PokedexPWA',
+    description: 'Pokedex now on PWA!',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'PokedexPWA - %s',
       meta: [
-        { name: 'description', content: 'All the modern best practices in one example.' },
+        { name: 'description', content: 'Pokedex now on PWA!' },
         { charset: 'utf-8' },
-        { property: 'og:site_name', content: 'React Redux Example' },
+        { property: 'og:site_name', content: 'PokedexPWA' },
         { property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg' },
         { property: 'og:locale', content: 'en_US' },
-        { property: 'og:title', content: 'React Redux Example' },
-        { property: 'og:description', content: 'All the modern best practices in one example.' },
+        { property: 'og:title', content: 'PokedexPWA' },
+        { property: 'og:description', content: 'Pokedex now on PWA!' },
         { property: 'og:card', content: 'summary' },
-        { property: 'og:site', content: '@erikras' },
-        { property: 'og:creator', content: '@erikras' },
+        { property: 'og:site', content: 'https://pokedex.arfianadam.com' },
+        { property: 'og:creator', content: '@arfianadam' },
         { property: 'og:image:width', content: '200' },
         { property: 'og:image:height', content: '200' }
       ]
