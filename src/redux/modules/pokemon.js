9bbbec0420c0fe47b54e71e42912cb8498e7fce9
Arfian Adam <arfianadam@yahoo.com>
2017-09-30 19:18:25 +0700

    disable auto recursively get pokemon details

--- src/redux/modules/pokemon.js
+++ src/redux/modules/pokemon.js
@@ -19,7 +19,7 @@ export default function reducer(state = initialState, action = {}) {
     case LOAD_SUCCESS:
       return {
         ...state,
-        pokemon: action.payload,
+        pokemon: [...state.pokemon, ...action.payload],
         loading: false
       };
     default:
@@ -40,15 +40,39 @@ function saveListPokemon(list) {
   };
 }
 
+export function loadDetailPokemon(url) {
+  return () => new Promise((resolve) => {
+    request.get(url, {}, true)
+      .then(res => {
+        resolve(res);
+      });
+  });
+}
+
+function mapPokemonId(pokemon, index) {
+  return {
+    ...pokemon,
+    id: index + 1
+  };
+}
+
 export function loadListPokemon(page = 1, limit = 60, toEnd = false) {
-  if (toEnd) {
-    return;
-  }
   return dispatch => {
     dispatch(startLoad());
     request.get(`/pokemon/?offset=${(page - 1) * limit}&limit=${limit}`)
       .then(res => {
-        dispatch(saveListPokemon(res.results));
+        dispatch(saveListPokemon(res.results.map(mapPokemonId)));
+        if (toEnd && res.next) {
+          dispatch(loadListPokemon(page + 1, 60, true));
+        }
+        // const promises = [];
+        // res.results.forEach(pokemon => {
+        //   promises.push(dispatch(loadDetailPokemon(pokemon.url)));
+        // });
+        // Promise.all(promises)
+        //   .then(completedPokemon => {
+        //     dispatch(saveListPokemon(completedPokemon));
+        //   });
       });
   };
 }
