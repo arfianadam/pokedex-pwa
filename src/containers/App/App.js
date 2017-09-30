7fdbeed7e85747386d811ebf333386013ad9fd39
Arfian Adam <arfianadam@yahoo.com>
2017-10-01 01:05:50 +0700

    now pokemons are preloaded on every route

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import config from 'config';
+import { loadListPokemon } from 'redux/modules/pokemon';
 
 import Header from 'containers/Header';
 
+@connect(state => ({
+  pokemon: state.pokemon
+}))
 export default class App extends Component {
   static propTypes = {
     children: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired
+    location: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    pokemon: PropTypes.object.isRequired
   };
+
+  componentDidMount() {
+    const { dispatch, pokemon } = this.props;
+    if (!pokemon.allLoaded) {
+      dispatch(loadListPokemon(1, 60, true));
+    }
+  }
 
   render() {
     const { children, location } = this.props;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,22 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import PokemonList from 'containers/PokemonList';
-import { loadListPokemon } from 'redux/modules/pokemon';
 
 @connect(state => ({
   pokemon: state.pokemon
 }))
 export default class Home extends Component {
   static propTypes = {
-    dispatch: PropTypes.func.isRequired,
     pokemon: PropTypes.object.isRequired
-  }
-
-  componentDidMount() {
-    const { dispatch, pokemon } = this.props;
-    if (!pokemon.allLoaded) {
-      dispatch(loadListPokemon(1, 60, true));
-    }
   }
 
   render() {
